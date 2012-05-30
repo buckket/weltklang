987cd8080063559f236ca75b30af4322cf615561
MrLoom <MrLoom@soeinfachistdas.net>
2012-05-30 21:03:59 +0200

    started webapi

--- lib/rfk/api/__init__.py
+++ lib/rfk/api/__init__.py
@@ -1,9 +1,12 @@
 import cherrypy
 from rfk.api.icecast import IcecastAPI
+from rfk.api.webapi import WebAPI
 
 class API(object):
     
     icecast = IcecastAPI()
+    web = WebAPI()
+    
     @cherrypy.expose
     def index(self):
         
