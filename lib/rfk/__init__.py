875c79569636f3c2fcd1139cf6015d388241b0df
teddydestodes <derkomtur@schattengang.net>
2013-03-16 21:37:53 +0100

    geoip

--- lib/rfk/__init__.py
+++ lib/rfk/__init__.py
@@ -1,8 +1,13 @@
 from ConfigParser import SafeConfigParser
 import os
+import pygeoip
 
 
 CONFIG = SafeConfigParser()
 
+geoip = None
+
 def init(basepath):
+    global geoip
     CONFIG.read(os.path.join(basepath,'etc', 'config.cfg'))
+    geoip = pygeoip.GeoIP(CONFIG.get('site', 'geoipdb'), pygeoip.MEMORY_CACHE)
--- lib/rfk/helper/__init__.py
+++ lib/rfk/helper/__init__.py
@@ -1,5 +1,9 @@
 import pytz
 import datetime
+import rfk
 
 def now():
     return pytz.utc.localize(datetime.datetime.utcnow())
+
+def get_location(address):
+    return rfk.geoip.record_by_addr(address)
