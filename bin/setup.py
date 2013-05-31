2fa199047f760a835011eefc7a59febc02fd4ee1
TeddyDesTodes <derkomtur@schattengang.net>
2013-05-31 17:22:30 +0200

    .

--- bin/setup.py
+++ bin/setup.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python
-'''
-Created on 30.04.2012
 
-@author: teddydestodes
-'''
+
 import os
 import sys
 
@@ -20,12 +16,9 @@ init_db("%s://%s:%s@%s/%s?charset=utf8" % (rfk.CONFIG.get('database', 'engine'),
                                                rfk.CONFIG.get('database', 'password'),
                                                rfk.CONFIG.get('database', 'host'),
                                                rfk.CONFIG.get('database', 'database')))
-from rfk.site import app
-app.template_folder = os.path.join(basedir,'var','template')
-app.static_folder = os.path.join(basedir,'web_static')
-app.config['BABEL_LOCALE_PATH'] = os.path.join(basedir,'var','translations')
-app.config['BASEDIR'] = basedir
-app.static_url_path = '/static'
+import rfk.install
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+rfk.install.setup_permissions()
+rfk.install.setup_settings()
+rfk.install.setup_default_user('admin', 'admin')
+rfk.database.session.commit()
---
+++ lib/rfk/install.py
@@ -0,0 +1,42 @@
+'''
+Created on May 31, 2013
+
+@author: teddydestodes
+'''
+
+from rfk.database.base import Permission, Setting, User
+import rfk.database
+
+
+def setup_settings():
+    settings = []
+    settings.append(Setting.add_setting('use_icy', 'Use ICY-Tags for unplanned Shows', Setting.TYPES.INT))
+    settings.append(Setting.add_setting('show_def_name', 'Default Name for new Shows', Setting.TYPES.STR))
+    settings.append(Setting.add_setting('show_def_desc', 'Default Description for new Shows', Setting.TYPES.STR))
+    settings.append(Setting.add_setting('show_def_tags', 'Default Tags for new Shows', Setting.TYPES.STR))
+    settings.append(Setting.add_setting('show_def_logo', 'Default Logo for Shows', Setting.TYPES.STR))
+    settings.append(Setting.add_setting('locale', 'Locale', Setting.TYPES.STR))
+    settings.append(Setting.add_setting('timezone', 'Timezone', Setting.TYPES.STR))
+    for setting in settings:
+        rfk.database.session.add(setting)
+    rfk.database.session.commit() 
+
+def setup_permissions():
+    permissions = []
+    permissions.append(Permission.add_permission('manage-liquidsoap', 'Manage Liquidsoap'))
+    permissions.append(Permission.add_permission('manage-relays', 'Manage Relays'))
+    permissions.append(Permission.add_permission('admin', 'Admin'))
+    for permission in permissions:
+        rfk.database.session.add(permission)
+    rfk.database.session.commit()
+    
+def setup_default_user(username, password):
+    users = []
+    users.append(User.add_user(username, password))
+    for user in users:
+        rfk.database.session.add(user)
+        user.add_permission(code='manage-liquidsoap')
+        user.add_permission(code='manage-relays')
+        user.add_permission(code='admin')
+        print "[users] Added %s" % user.username
+    rfk.database.session.commit()
