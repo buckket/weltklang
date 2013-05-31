f418be8262798cd1e96ecdc7fc4c5a0a2836c571
TeddyDesTodes <derkomtur@schattengang.net>
2013-05-31 16:11:11 +0200

    modified rfk_web to run with uwsgi

--- bin/rfk_web.py
+++ bin/rfk_web.py
@@ -4,22 +4,30 @@ Created on 30.04.2012
 
 @author: teddydestodes
 '''
+
+
 import os
+import sys
+
+basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(basedir,'lib'))
+
 import rfk
 from rfk.database import init_db
 
-if __name__ == '__main__':
-    current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    rfk.init(current_dir)
-    init_db("%s://%s:%s@%s/%s?charset=utf8" % (rfk.CONFIG.get('database', 'engine'),
+
+rfk.init(basedir)
+init_db("%s://%s:%s@%s/%s?charset=utf8" % (rfk.CONFIG.get('database', 'engine'),
                                                rfk.CONFIG.get('database', 'username'),
                                                rfk.CONFIG.get('database', 'password'),
                                                rfk.CONFIG.get('database', 'host'),
                                                rfk.CONFIG.get('database', 'database')))
-    import rfk.site
-    rfk.site.app.template_folder = os.path.join(current_dir,'var','template')
-    rfk.site.app.static_folder = os.path.join(current_dir,'web_static')
-    rfk.site.app.config['BABEL_LOCALE_PATH'] = os.path.join(current_dir,'var','translations')
-    rfk.site.app.config['BASEDIR'] = current_dir
-    rfk.site.app.static_url_path = '/static'
+import rfk.site
+rfk.site.app.template_folder = os.path.join(basedir,'var','template')
+rfk.site.app.static_folder = os.path.join(basedir,'web_static')
+rfk.site.app.config['BABEL_LOCALE_PATH'] = os.path.join(basedir,'var','translations')
+rfk.site.app.config['BASEDIR'] = basedir
+rfk.site.app.static_url_path = '/static'
+
+if __name__ == '__main__':
     rfk.site.app.run(host='0.0.0.0', debug=True)
