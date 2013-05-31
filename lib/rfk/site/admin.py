9eb98990b4c07df82719ac7720ca3e342f562db9
TeddyDesTodes <derkomtur@schattengang.net>
2013-05-31 17:35:59 +0200

    moved some files

--- lib/rfk/site/admin.py
+++ lib/rfk/site/admin.py
@@ -6,6 +6,8 @@ Created on Aug 11, 2012
 from flask import Blueprint, render_template, url_for
 from functools import wraps
 import rfk
+import rfk.liquidsoap
+import rfk.site
 from rfk.site.helper import permission_required
 from flask.ext.login import login_required, current_user
 
@@ -22,19 +24,27 @@ def index():
 def streams():
     return render_template('admin/streams.html')
 
-@admin.route('/liquidsoap')
+@admin.route('/liquidsoap/manage')
 @login_required
-def liquidsoap():
-    return render_template('admin/liquidsoap.html')
+@permission_required(permission='manage-liquidsoap')
+def liquidsoap_manage():
+    return render_template('admin/liquidsoap/management.html')
 
+@admin.route('/liquidsoap/config')
+@login_required
+@permission_required(permission='manage-liquidsoap')
+def liquidsoap_config():
+    config = rfk.liquidsoap.gen_script(rfk.site.app.config['BASEDIR']).encode('utf-8')
+    return render_template('admin/liquidsoap/config.html', liq_config=config)
 
 def create_menu(endpoint):
     if not current_user.has_permission(code='admin'):
         return False
     menu = {'name': 'Admin', 'submenu': [], 'active': False}
     entries = []
-    if current_user.has_permission(code='liq-restart'):
-        entries.append(['admin.liquidsoap', 'Liquidsoap', 'admin'])
+    if current_user.has_permission(code='manage-liquidsoap'):
+        entries.append(['admin.liquidsoap_manage', 'Liquidsoapmanager', 'admin'])
+        entries.append(['admin.liquidsoap_config', 'Liquidsoapconfig', 'admin'])
     for entry in entries:
         active = endpoint == entry[0]
         menu['submenu'].append({'name': entry[1],
