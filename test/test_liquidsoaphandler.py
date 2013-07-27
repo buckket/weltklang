58179b40ab847c19440772206ab7b2aa384e0abb
TeddyDesTodes <derkomtur@schattengang.net>
2013-07-27 11:38:25 +0200

    tests

--- test/test_liquidsoaphandler.py
+++ test/test_liquidsoaphandler.py
@@ -13,15 +13,18 @@ import rfk.liquidsoaphandler
 from rfk.install import setup_settings
 from rfk.helper import now
 from datetime import timedelta
+from lib.rfk import liquidsoaphandler
+from rfk.exc.base import UserNameTakenException
 
 class Test(unittest.TestCase):
 
     def setUp(self):
-        rfk.database.init_db('sqlite://', True)
+        rfk.init()
+        rfk.database.init_db("sqlite://")
         setup_settings()
         try:
             self.user = User.add_user('teddydestodes', 'roflmaoblubb')
-            User.add_user('test', 'test')
+            self.other_user = User.add_user('test', 'test')
         except UserNameTakenException:
             pass
         rfk.database.session.commit()
@@ -59,7 +62,7 @@ class Test(unittest.TestCase):
         self.test_do_connect_valid_user()
         data = {"album": "Pedestrian Verse", "userid": "1", "title": "Late March, Death March", "vendor": "Xiph.Org libVorbis I 20101101 (Schaufenugget)", "artist": "Frightened Rabbit"}
         rfk.liquidsoaphandler.doMetaData(data)
-        print Track.current_track()
+        self.assertNotEqual(Track.current_track(),None)
     
     def test_init_show_planned(self):
         begin = now()-timedelta(minutes=10)
@@ -75,10 +78,45 @@ class Test(unittest.TestCase):
         rfk.database.session.commit()
         self.test_do_connect_valid_user()
         self.assertEqual(show, show.get_active_show())
-        
+        self.assertEqual(Show.get_active_show().flags & Show.FLAGS.PLANNED, Show.FLAGS.PLANNED)
     
     def test_init_show_unplanned(self):
-        pass
+        self.test_do_connect_valid_user()
+        self.test_do_metadata()
+        self.assertEqual(Show.get_active_show().flags & Show.FLAGS.UNPLANNED, Show.FLAGS.UNPLANNED)
+        
+    def test_transition_planned_unplanned(self):
+        self.test_init_show_planned()
+        show = Show.get_active_show()
+        show.end = show.end - timedelta(minutes=15)
+        rfk.database.session.commit()
+        liquidsoaphandler.init_show(self.user)
+        self.assertNotEqual(show, Show.get_active_show())
+        self.assertEqual(Show.get_active_show().flags & Show.FLAGS.UNPLANNED, Show.FLAGS.UNPLANNED)
+    
+    def test_transition_unplanned_planned(self):
+        self.test_init_show_unplanned()
+        show = Show.get_active_show()
+        show.begin = show.begin - timedelta(minutes=5)
+        rfk.database.session.commit()
+        begin = now()-timedelta(minutes=3)
+        end = now()+timedelta(minutes=10)
+        show = Show(begin=begin,
+                    end=end,
+                    name='titel2',
+                    description='description2',
+                    flags=Show.FLAGS.PLANNED)
+        rfk.database.session.add(show)
+        rfk.database.session.flush()
+        show.add_user(self.other_user)
+        rfk.database.session.commit()
+        self.test_do_metadata()
+        self.assertEqual(Show.get_active_show(), show)
+        
+    def test_disconnect(self):
+        liquidsoaphandler.doDisconnect(1)
+        self.assertEqual(Show.get_active_show(), None)
+        self.assertEqual(Track.current_track(), None)
         
         
 if __name__ == "__main__":
