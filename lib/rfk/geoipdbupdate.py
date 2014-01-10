72e642bc2058fae3d2e8ec030724eeb795bacb46
buckket <MrLoom@soeinfachistdas.net>
2014-01-10 21:56:45 +0100

    added a geoipdb update script

---
+++ lib/rfk/geoipdbupdate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+import rfk
+import shutil
+import urllib
+import subprocess
+
+
+def main():
+    rfk.init()
+
+    (file_name, headers) = urllib.urlretrieve('http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz')
+    ret_val = subprocess.call(['gunzip', file_name])
+
+    if ret_val == 0:
+        shutil.move(file_name[:-3], rfk.CONFIG.get('site', 'geoipdb'))
+
+if __name__ == '__main__':
+    sys.exit(main())
