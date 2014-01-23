22d0e58f99b5fc64af363bf60298b7c27f51ea78
buckket <MrLoom@soeinfachistdas.net>
2014-01-23 22:27:18 +0100

    Ich putze hier nur (Phase IV)

--- lib/rfk/exc/base.py
+++ lib/rfk/exc/base.py
@@ -5,16 +5,18 @@ class NoConfigException(Exception):
 class UserNotFoundException(Exception):
     pass
 
+
 class UserNameTakenException(Exception):
     pass
+
 
 class InvalidPasswordException(Exception):
     pass
 
+
 class InvalidSettingException(Exception):
-    
     def __init__(self, reason):
         self.reason = reason
-        
+
     def __repr__(self):
         "<rfk.exc.base.InvalidSettingException %s>" % (self.reason,)
