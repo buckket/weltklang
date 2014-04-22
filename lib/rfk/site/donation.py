bbeb023f02ea8fecfebbdc12149358a67552e0c9
teddydestodes <derkomtur@schattengang.net>
2014-04-22 20:02:42 +0200

    added Menu entry

--- lib/rfk/site/donation.py
+++ lib/rfk/site/donation.py
@@ -1,28 +1,30 @@
 import datetime
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, url_for
+from rfk.database.donations import Donation
 __author__ = 'teddydestodes'
 
 donation = Blueprint('donation', __name__)
 
 try:
     from rfk.armor import get_serviceproxy
-    @donation.route('/list')
+    @donation.route('/')
     def list():
         sp = get_serviceproxy()
         transactions = []
         for transaction in sp.listtransactions():
             print transaction
-            if transaction['category'] != 'recieve': # no one should ever know
-                # not comments, need to patch armord to fetch comments and put it into transaction['comment']
+            if transaction['category'] != 'recieve':  # no one should ever know
                 transactions.append({'timestamp': datetime.datetime.fromtimestamp(transaction['time']),
                                      'amount': transaction['amount'],
                                      'currency': 'BTC',
                                      'comment': ''})
         return render_template('donations.html', donations=transactions)
 
+    def create_menu(endpoint):
+        menu = {'name': 'Donations',
+                'active': endpoint == 'donation.list',
+                'url': url_for('donation.list')}
+        return menu
 except ImportError:
-    @donation.route('/list')
-    def list():
-        return 'i am sorry;_;'
+    pass
 
