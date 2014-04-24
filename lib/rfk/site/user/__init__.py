0430ddb7ff43729718e30cfc042fdf797e510ec9
buckket <MrLoom@soeinfachistdas.net>
2014-04-24 23:11:14 +0200

    reworked user page

--- lib/rfk/site/user/__init__.py
+++ lib/rfk/site/user/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, abort
 
-from rfk.helper import now, iso_country_to_countryball
+import rfk.exc
+
+from rfk.helper import now
 from rfk.database.base import User
 from rfk.database.show import Show, UserShow
 
@@ -10,18 +12,33 @@ import apikey
 
 @user.route('/<user>')
 def info(user):
-    user = User.get_user(username=user)
+    try:
+        user = User.get_user(username=user)
+    except rfk.exc.base.UserNotFoundException:
+        abort(404)
 
+    # checking user rank
+    if user.has_permission('admin'):
+        rank = 'Admin'
+    else:
+        rank = 'User'
+
+    # count planned and unplanned shows
+    planned_shows = Show.query.join(UserShow).filter(UserShow.user == user, Show.flags == Show.FLAGS.PLANNED).count()
+    unplanned_shows = Show.query.join(UserShow).filter(UserShow.user == user, Show.flags == Show.FLAGS.UNPLANNED).count()
+    total_shows = planned_shows + unplanned_shows
+
+    # list upcoming and recent shows
     upcoming_shows = Show.query.join(UserShow).filter(UserShow.user == user, Show.begin >= now()).order_by(
         Show.begin.asc()).limit(5).all()
     last_shows = Show.query.join(UserShow).filter(UserShow.user == user, Show.end <= now()).order_by(
         Show.end.desc()).limit(5).all()
-    if user:
-        ball = iso_country_to_countryball(user.country)
-        return render_template('user/info.html',
-                               username=user.username,
-                               ball=ball,
-                               st=user.get_total_streamtime(),
-                               shows={'upcoming': upcoming_shows, 'last': last_shows})
-    else:
-        abort(404)
+
+    return render_template('user/info.html', TITLE=user.username,
+                           user=user,
+                           rank=rank,
+                           planned_shows=planned_shows,
+                           unplanned_shows=unplanned_shows,
+                           total_shows=total_shows,
+                           st=user.get_total_streamtime(),
+                           shows={'upcoming': upcoming_shows, 'last': last_shows})
