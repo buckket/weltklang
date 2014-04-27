3ca1e72eae55da6ad3c83ba9dc3fccee740af9a9
buckket <MrLoom@soeinfachistdas.net>
2014-04-27 05:35:11 +0200

    finished stats magic

--- lib/rfk/userstats.py
+++ lib/rfk/userstats.py
@@ -2,6 +2,8 @@
 
 import sys
 
+from sqlalchemy.orm import exc
+
 import rfk
 import rfk.database
 from rfk.helper import now
@@ -9,15 +11,45 @@ from rfk.database import init_db
 from rfk.database.base import User
 from rfk.database.show import Show, UserShow, Role
 from rfk.database.streaming import Listener
+from rfk.database.stats import Statistic
 from rfk.database.stats import UserStatistic
+
+
+def generate_listener_peak(user):
+    role_host = Role.get_role('host')
+    user_listener_max = 0
+
+    shows = Show.query.join(UserShow).filter(UserShow.user == user,
+                                             UserShow.role == role_host,
+                                             UserShow.status == UserShow.STATUS.STREAMED,
+                                             Show.end <= now()).yield_per(50)
+
+    for show in shows:
+        try:
+            stats_total = Statistic.query.filter(Statistic.identifier == 'lst-total').one()
+            stats = stats_total.get(start=show.begin, stop=show.end)
+        except exc.NoResultFound:
+            stats = None
+
+        if stats and stats.count() > 0:
+            for stat in stats:
+                if stat.value > user_listener_max:
+                    user_listener_max = stat.value
+
+    if user_listener_max > 0:
+        us = UserStatistic.get_userstatistic(user, 'listenerpeak')
+        us.statistic.set(now(), user_listener_max)
+        rfk.database.session.flush()
 
 
 def generate_listenerhours(user):
     role_host = Role.get_role('host')
     total_listener_hours = 0
+
     for usershow in UserShow.query.filter(UserShow.user == user,
                                           UserShow.role == role_host,
                                           UserShow.status == UserShow.STATUS.STREAMED).yield_per(50):
+
         for listener in Listener.query.filter(Listener.connect <= usershow.show.end,
                                               usershow.show.begin <= Listener.disconnect).all():
             s = listener.connect
@@ -42,9 +74,12 @@ def generate_missed_show_ratio(user):
     role_host = Role.get_role('host')
     missed_shows = 0
     total_shows = 0
+
     for usershow in UserShow.query.join(Show).filter(UserShow.user == user,
                                                      UserShow.role == role_host,
+                                                     Show.flags == Show.FLAGS.PLANNED,
                                                      Show.end <= now()).yield_per(50):
+
         total_shows += 1
         if usershow.status != UserShow.STATUS.STREAMED:
             missed_shows += 1
@@ -56,7 +91,9 @@ def generate_missed_show_ratio(user):
         us.statistic.set(now(), missed_show_ratio)
         rfk.database.session.flush()
 
-stats = [generate_listenerhours,
+
+stats = [generate_listener_peak,
+         generate_listenerhours,
          generate_missed_show_ratio]
 
 
