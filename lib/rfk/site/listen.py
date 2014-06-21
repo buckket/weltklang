4f75f46c031ef917133486f3851623c11d459f67
buckket <MrLoom@soeinfachistdas.net>
2014-06-21 04:07:55 +0200

    snuff

--- lib/rfk/site/listen.py
+++ lib/rfk/site/listen.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, make_response
 from flask.templating import render_template
 
-
 import rfk
 from rfk.database.streaming import Stream
+
 
 listen = Blueprint('listen', __name__)
 
@@ -15,28 +15,29 @@ def html5_player():
 
 @listen.route('/<stream>')
 def playlist(stream):
+    """Return a m3u playlist pointing to our load balancer"""
+
     stream = Stream.query.filter(Stream.code == stream).first()
     if stream is None:
-        return make_response('I\'m sorry ;_;', 404)
+        return make_response('I\'m sorry ;_; (Invalid stream)', 404)
     m3u = "#EXTM3U\r\n"
     m3u += "#EXTINF:0, Radio freies Krautchan %s\r\n" % stream.name
     m3u += "http://%s/listen/stream/%s\r\n" % (rfk.CONFIG.get('site', 'url'), stream.stream)
-    return make_response(m3u, 200, {'Content-Type': 'audio/x-mpegurl',
+    return make_response(m3u, 200, {'Content-Type': 'application/x-mpegurl',
                                     'Content-Disposition': 'attachment; filename="%s.m3u"' % stream.mount[1:]})
 
 
 @listen.route('/stream/<stream>')
 def stream(stream):
-    """
-    redirect listener to best relay
-    """
+    """Redirect listener to relay with least traffic usage"""
+
     stream = Stream.query.get(int(stream))
     if stream is None:
-        return make_response('I\'m sorry ;_;', 404)
+        return make_response('I\'m sorry ;_; (Invalid stream)', 404)
 
     relay = stream.get_relay()
     if relay is None:
-        return make_response('I\'m sorry ;_; no suitable relays found', 503)
+        return make_response('I\'m sorry ;_; (No suitable relays found)', 503)
 
     address = relay.address
-    return make_response('', 301, {'Location': "http://%s:%s%s" % (address, relay.port, stream.mount), 'X-LOAD': 0})
+    return make_response('', 301, {'Location': "http://%s:%s%s" % (address, relay.port, stream.mount), 'X-LOAD': round(relay.get_load(), 2)})
