a274294aa5107d4560b5125c7e4713685d40437d
buckket <MrLoom@soeinfachistdas.net>
2014-06-21 02:59:29 +0200

    reworked geoipdbupdate script (now requires progressbar module)

--- lib/rfk/geoipdbupdate.py
+++ lib/rfk/geoipdbupdate.py
@@ -4,6 +4,7 @@ import sys
 import shutil
 import urllib
 import subprocess
+import progressbar
 
 import rfk
 
@@ -11,12 +12,20 @@ import rfk
 def main():
     rfk.init(enable_geoip=False)
 
-    db_url = 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz'
-    (file_name, headers) = urllib.urlretrieve(db_url)
-    ret_val = subprocess.call(['gunzip', file_name])
+    def download_progress(blocks_read, block_size, total_size):
+        if blocks_read == 0:
+            widgets = [progressbar.Percentage(), ' ', progressbar.Bar(), ' ', progressbar.ETA(), ' ', progressbar.FileTransferSpeed()]
+            download_progress.progress_bar = progressbar.ProgressBar(widgets=widgets, maxval=(total_size/block_size) + 1).start()
+        download_progress.progress_bar.update(blocks_read)
 
+    db_url = 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz'
+    (file_name, headers) = urllib.urlretrieve(db_url, reporthook=download_progress)
+
+    ret_val = subprocess.call(['gunzip', file_name])
     if ret_val == 0:
         shutil.move(file_name[:-3], rfk.CONFIG.get('site', 'geoipdb'))
+    else:
+        raise IOError
 
 
 if __name__ == '__main__':
